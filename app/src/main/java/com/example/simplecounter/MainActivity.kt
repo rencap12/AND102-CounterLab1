d4b6c5c1faa6828d0b8e414950ed15088dae758d
Reneca Capuno <capunoreneca@gmail.com>
2025-10-03 20:20:16 -0400

    added plus 2 functionality

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.simplecounter
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -19,13 +20,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.textView3)
-
+        val upgradeButton = findViewById<Button>(R.id.button2)
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener{
             counter++
             textView.text = counter.toString()
-            Toast.makeText(it.context, "clicked add 1", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(it.context, "clicked add 1", Toast.LENGTH_SHORT).show()
+
+            if (counter >= 100) {
+                upgradeButton.visibility = View.VISIBLE
+                upgradeButton.setOnClickListener{
+                    button.text = "Add 2"
+
+                    button.setOnClickListener({
+                        counter += 2
+                        textView.text = counter.toString()
+                    })
+
+                    upgradeButton.visibility = View.INVISIBLE
+                }
+            }
+
         }
+
 
     }
 }
