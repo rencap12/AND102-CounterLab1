743bad5cc60af9a2ea2543cadbe28df549576a26
Reneca Capuno <capunoreneca@gmail.com>
2025-10-03 20:09:42 -0400

    finished initial counter

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.simplecounter
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val textView = findViewById<TextView>(R.id.textView3)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            counter++
+            textView.text = counter.toString()
+            Toast.makeText(it.context, "clicked add 1", Toast.LENGTH_SHORT).show()
         }
+
     }
 }
